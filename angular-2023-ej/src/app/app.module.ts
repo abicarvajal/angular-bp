9d07103d2c86cb2bdca90cdef5cd72e4fbf09e80
Juan Carlos Echenique Martinez <juan.echenique@neoris.com>
2023-03-17 12:19:02 -0500

    avance dashboard

--- angular-2023-ej/src/app/app.module.ts
+++ angular-2023-ej/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidemenuComponent } from './components/sidemenu/sidemenu.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { RidesComponent } from './pages/rides/rides.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidemenuComponent,
+    DashboardComponent,
+    RidesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
